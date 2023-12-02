281ab346d8f517fc63f7a8316671c6aa2d258273
darianfd99 <darianfd99@gmail.com>
2023-12-02 14:31:44 +0300

    fix(xargs) fix compile error

--- user/xargs.c
+++ user/xargs.c
@@ -13,7 +13,7 @@ int main(int argc, char* argv[]){
     }
 
     int pid, n, i = 0;
-    char buf, arg[1024]
+    char buf, arg[1024];
     char *args[50];
 
     for(int i = 1; i < argc; ++i) args[i-1] = argv[i];
