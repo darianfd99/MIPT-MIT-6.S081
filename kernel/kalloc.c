6b7ae75a8fa34c97e7bca64a0b21b6201eb562b1
darianfd99 <darianfd99@gmail.com>
2023-12-21 02:53:39 +0300

    feat(cow) add copy-on-write implementation

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,14 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
+  int refs[PHYSTOP/PGSIZE];
 } kmem;
 
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+  memset(kmem.refs, 0, sizeof(kmem.refs)/sizeof(int));
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -51,14 +53,13 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
-
-  r = (struct run*)pa;
-
   acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
+  if((--kmem.refs[REFCOUNTINDEX(pa)]) <= 0){
+      memset(pa, 1, PGSIZE);
+      r = (struct run*) pa;
+      r->next = kmem.freelist;
+      kmem.freelist = r;
+  }
   release(&kmem.lock);
 }
 
@@ -72,11 +73,53 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
+  if(r) {
+      kmem.freelist = r->next;
+      kmem.refs[REFCOUNTINDEX(r)] = 1;
+  }
   release(&kmem.lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
+}
+
+void addref(void *pa){
+    acquire(&kmem.lock);
+    kmem.refs[REFCOUNTINDEX(pa)] += 1;
+    release(&kmem.lock);
+}
+
+int handlepagefault(pagetable_t pt, uint64 va){
+    if(va >= MAXVA) return -1;
+
+    pte_t *pte = walk(pt, va, 0);
+    if(pte == 0)
+        return -1;
+    if((*pte & PTE_V) == 0)
+        return -1;
+    if((*pte & PTE_U) == 0)
+        return -1;
+
+    uint64 pa = (uint64) PTE2PA(*pte);
+
+    acquire(&kmem.lock);
+    if(kmem.refs[REFCOUNTINDEX(pa)] == 1){
+        *pte &= ~PTE_COW;
+        *pte |= PTE_W;
+        release(&kmem.lock);
+        return 0;
+    }
+    release(&kmem.lock);
+
+    uint64 mem;
+    if((mem = (uint64)kalloc()) == 0) return -1;
+    memmove((void *)mem, (void *)pa, PGSIZE);
+    kfree((void *)pa);
+
+    *pte = PA2PTE(mem) | PTE_FLAGS(*pte);
+    *pte &= ~PTE_COW;
+    *pte |= PTE_W;
+
+    return 0;
 }
